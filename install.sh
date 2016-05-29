293ab5da8563ea013ae234d83fbeb0bbd8a145ff
Senthil Kumaran <senthil@uthcode.com>
2016-05-28 17:33:44 -0700

    Updated with more configuration files.

--- install.sh
+++ install.sh
@@ -3,20 +3,23 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # vim
-ln -f -s ${BASEDIR}/vimrc ~/.vimrc
+ln -f -s "${BASEDIR}"/vimrc ~/.vimrc
 
 # git
-ln -f -s ${BASEDIR}/gitconfig ~/.gitconfig
+ln -f -s "${BASEDIR}"/gitconfig ~/.gitconfig
 
 # bashrc
-ln -f -s ${BASEDIR}/bashrc ~/.bashrc
+ln -f -s "${BASEDIR}"/bashrc ~/.bashrc
 
 # bash_profile
-ln -f -s ${BASEDIR}/bash_profile ~/.bash_profile
+ln -f -s "${BASEDIR}"/bash_profile ~/.bash_profile
 
 # muttrc
-ln -f -s ${BASEDIR}/muttrc.colors ~/.muttrc.colors
-ln -f -s ${BASEDIR}/muttrc ~/.muttrc
+ln -f -s "${BASEDIR}"/muttrc.colors ~/.muttrc.colors
+ln -f -s "${BASEDIR}"/muttrc ~/.muttrc
 
 # Xdefaults for x2go
-ln -f -s ${BASEDIR}/Xdefaults ~/.Xdefaults
+ln -f -s "${BASEDIR}"/Xdefaults ~/.Xdefaults
+
+# isort
+ln -f -s "${BASEDIR}"/isort.cfg ~/.isort.cfg
